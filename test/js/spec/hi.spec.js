efb30260fa7673f62ccf3aebc1e48bd86c752ec7
David G Chaves <davidgchaves@gmail.com>
2014-06-18 09:49:47 +0200

    Mocha and Chai work together

--- test/js/spec/hi.spec.js
+++ test/js/spec/hi.spec.js
@@ -2,3 +2,11 @@ window.hi = function () {
   return "Hi from js-world!";
 };
 
+describe("The testing libraries are wired up properly", function () {
+  describe("Mocha and Chai", function () {
+    it("work together", function () {
+      expect(hi()).to.equal("Hi from js-world!");
+    });
+  });
+});
+
