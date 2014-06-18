f999124869feaf55c60eb45d9725f43e8b88e493
David G Chaves <davidgchaves@gmail.com>
2014-06-18 09:58:52 +0200

    Mocha, Chai and Sinon work together

--- test/js/spec/hi.spec.js
+++ test/js/spec/hi.spec.js
@@ -8,5 +8,16 @@ describe("The testing libraries are wired up properly", function () {
       expect(hi()).to.equal("Hi from js-world!");
     });
   });
+
+  describe("Mocha, Chai and Sinon", function () {
+    it("work together", function () {
+      var hiSinonSpy = sinon.spy(window, "hi");
+
+      hi();
+      expect(hiSinonSpy.called).to.be.true;
+
+      hi.restore();
+    });
+  });
 });
 
