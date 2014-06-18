b2c7584629a73f4eb2b348dd2c9c5e1e82aca726
David G Chaves <davidgchaves@gmail.com>
2014-06-18 13:21:26 +0200

    Move hi() function to its own file   Everything wired up. Working as expected.

--- test/js/spec/hi.spec.js
+++ test/js/spec/hi.spec.js
@@ -1,7 +1,3 @@
-window.hi = function () {
-  return "Hi from js-world!";
-};
-
 describe("The testing libraries are wired up properly", function () {
   describe("Mocha and Chai", function () {
     it("work together", function () {
